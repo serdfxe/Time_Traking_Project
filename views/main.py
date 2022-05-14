0e2bfff482b7a5c43a04f5594e3d5e49be503edb
pavel <pavel@DESKTOP-33FO31S>
2022-05-14 15:59:26 +0300

    master

--- views/main.py
+++ views/main.py
@@ -7,7 +7,7 @@ main = Blueprint("main", __name__)
 
 @main.route("/<s>", methods=('GET', 'POST'))
 def root_page(s):
-    return render_template(urls_to_files[s], side_bar_components=side_bar_components, current=s)
+    return render_template(urls_to_files[s], side_bar_components=side_bar_components, side_bar_logos=side_bar_logos, current=s)
 
 
 @main.route("/", methods=('GET', 'POST'))
